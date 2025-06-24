e1f54462be33a8911b3592b4d02f9f7d0919e253
biancaiurascu <iurascuandreea27@gmail.com>
2025-06-24 23:32:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,191 @@
+﻿
+using System;
+using System.Runtime.CompilerServices;
+
+namespace HellWorld2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a;
+            //a = 12;
+            //console.writeline(a);
+            //console.writeline("new value for a");
+            //a = int.parse(console.readline());
+            //console.writeline("another value for a");
+            //string input = console.readline();
+            //if (int.tryparse(input, out int number))
+            //    console.write(number);
+            //else
+            //    console.write("bad input");
+            //console.writeline("min value for a");
+            //console.writeline(int32.minvalue);
+            //console.writeline("max value for a");
+            //console.writeline(int32.maxvalue);
+
+            //byte b;
+            //b = 20;
+            //Console.WriteLine(b);
+            //Console.WriteLine("New value for a");
+            //b = byte.Parse(Console.ReadLine());
+            //Console.WriteLine("Another value for b");
+            //string input = Console.ReadLine();
+            //if (byte.TryParse(input, out byte number))
+            //    Console.Write(number);
+            //else
+            //    Console.Write("Bad input");
+            //Console.WriteLine(" Min value for b");
+            //Console.WriteLine(Byte.MinValue);
+            //Console.WriteLine("Max value for b");
+            //Console.WriteLine(Byte.MaxValue);
+
+            //sbyte c;
+            //c = -50;
+            //Console.WriteLine(c);
+            //Console.WriteLine("New value for c");
+            //c = sbyte.Parse(Console.ReadLine());
+            //Console.WriteLine("Another value for c");
+            //string input = Console.ReadLine();
+            //if (sbyte.TryParse(input, out sbyte number))
+            //    Console.Write(number);
+            //else
+            //    Console.Write("Bad input");
+            //Console.WriteLine("Min value for c");
+            //Console.WriteLine(SByte.MinValue);
+            //Console.WriteLine("Max value for c");
+            //Console.WriteLine(SByte.MaxValue);
+
+            //short d;
+            //d = 1000;
+            //Console.WriteLine(d);
+            //Console.WriteLine("New value for d");
+            //d = short.Parse(Console.ReadLine());
+            //Console.WriteLine("Another value for d");
+            //string input = Console.ReadLine();
+            //if (short.TryParse(input, out short number))
+            //    Console.Write(number);
+            //else
+            //    Console.Write("Bad input");
+            //Console.WriteLine("Min value for d");
+            //Console.WriteLine(short.MinValue);
+            //Console.WriteLine("Max value for d");
+            //Console.WriteLine(short.MaxValue);
+
+            //ushort e;
+            //e = 6400;
+            //Console.WriteLine(e);
+            //Console.WriteLine("New value for e");
+            //e = ushort.Parse(Console.ReadLine());
+            //Console.WriteLine("Another value for e");
+            //string input = Console.ReadLine();
+            //if (ushort.TryParse(input, out ushort number))
+            //    Console.Write(number);
+            //else
+            //    Console.WriteLine("Bad input");
+            //Console.WriteLine("Min value for e");
+            //Console.WriteLine(ushort.MinValue);
+            //Console.WriteLine("Max value for e");
+            //Console.WriteLine(ushort.MaxValue);
+
+            //uint f;
+            //f = 1000000000;
+            //Console.WriteLine(f);
+            //Console.WriteLine("New value for f");
+            //f = uint.Parse(Console.ReadLine());
+            //Console.WriteLine("Another value for f");
+            //string input = Console.ReadLine();
+            //if (uint.TryParse(input, out uint number))
+            //    Console.Write(number);
+            //else
+            //    Console.WriteLine("Bad input");
+            //Console.WriteLine("Min value for f");
+            //Console.WriteLine(UInt16.MinValue);
+            //Console.WriteLine("Max value for f");
+            //Console.WriteLine(UInt16.MaxValue);
+
+            //long g;
+            //g = 57356899;
+            //Console.WriteLine(g);
+            //Console.WriteLine("New value for g");
+            //g = long.Parse(Console.ReadLine());
+            //Console.WriteLine("Another value for g");
+            //string input = Console.ReadLine();
+            //if (long.TryParse(input, out long number))
+            //    Console.Write(number);
+            //else
+            //    Console.WriteLine("Bad input");
+            //Console.WriteLine("Min value for g");
+            //Console.WriteLine(Int64.MinValue);
+            //Console.WriteLine("Max value for g");
+            //Console.WriteLine(Int64.MaxValue);
+
+            //ulong h;
+            //h = 754689;
+            //Console.WriteLine(h);
+            //Console.WriteLine("New value for h");
+            //h = ulong.Parse(Console.ReadLine());
+            //Console.WriteLine("Another value for h");
+            //string input = Console.ReadLine();
+            //if (long.TryParse(input, out long number))
+            //    Console.Write(number);
+            //else
+            //    Console.WriteLine("Bad input");
+            //Console.WriteLine("Min value for h");
+            //Console.WriteLine(UInt64.MinValue);
+            //Console.WriteLine("Max value for h");
+            //Console.WriteLine(UInt64.MaxValue);
+
+            //float i;
+            //i = 0.00000000567f;
+            //Console.WriteLine(i);
+            //Console.WriteLine("New value for i");
+            //i = float.Parse(Console.ReadLine());
+            //Console.WriteLine("Another value for i");
+            //string input = Console.ReadLine();
+            //if (float.TryParse(input, out float number))
+            //    Console.Write(number);
+            //else
+            //    Console.WriteLine("Bad input");
+            //Console.WriteLine("Min value for i");
+            //Console.WriteLine(Single.MinValue);
+            //Console.WriteLine("Max value for i");
+            //Console.WriteLine(Single.MaxValue);
+
+            //double j;
+            //j = 0.55555555555566735446;
+            //Console.WriteLine(j);
+            //Console.WriteLine("New value for j");
+            //j = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Another value for j");
+            //string input = Console.ReadLine();
+            //if (double.TryParse(input, out double number))
+            //    Console.Write(number);
+            //else
+            //    Console.WriteLine("Bad input");
+            //Console.WriteLine("Min value for j");
+            //Console.WriteLine(Double.MinValue);
+            //Console.WriteLine("Max value for j");
+            //Console.WriteLine(Double.MaxValue);
+
+            decimal k;
+            k = 1.234444444m;
+            Console.WriteLine(k);
+            Console.WriteLine("New value for k");
+            k = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Another value for k");
+            string input = Console.ReadLine();
+            if (decimal.TryParse(input, out decimal number))
+                Console.Write(number);
+            else
+                Console.WriteLine("Bad input");
+            Console.WriteLine("Min value for k");
+            Console.WriteLine(Decimal.MinValue);
+            Console.WriteLine("Max value for k");
+            Console.WriteLine(Decimal.MaxValue);
+
+
+        
+         }
+    }
+}
