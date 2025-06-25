579ffe3f349f5f5a31af146b7af73cda9095c7ea
Mihai Bariz <barizmihai95@gmail.com>
2025-06-25 15:12:06 +0300

    Update and rename Program.cs to HW2.cs

---
+++ HW2.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+
+int error = 334;
+string VarType = "stalled";
+Console.Write("Error {0}: ", error);
+Console.WriteLine($"Variable 'VarType' is of '{VarType}' type and must be 'ready', 'busy', or 'error', ");
+
+
+int error_1 = 222;
+string user = "unknown";
+Console.Write($"Error {error_1}: ");
+Console.WriteLine("Variable 'user' is of '{0}' type and must be 'admin', 'operator' or 'viewer'.", user);
+
+
+int error_2 = 4473;
+string var = "dangerous";
+Console.Write($"Error {error_2}: ");
+Console.WriteLine($"Variable 'var' is of '{var}' type and must be 'normal', 'debug', 'safe'.");
